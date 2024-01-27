4fabb5fb520a24249db8e6a8c60cae02bd2ca9e8
kevin.garcia <kevin.garcia@sinapsis.global>
2024-01-27 17:02:39 -0500

    fix line bug

--- src/Components/Shared/LogicCard/LogicCard.tsx
+++ src/Components/Shared/LogicCard/LogicCard.tsx
@@ -1,7 +1,6 @@
 import "./LogicCard.scss";
 
 const LogicCard = (prop: any) => {
-  console.log("🚀 ~ LogicCard ~ item:", prop.item);
   return (
     <>
       <h3 className="title">{prop.item.title}</h3>
--- src/Components/Shared/Rope/Rope.tsx
+++ src/Components/Shared/Rope/Rope.tsx
@@ -1,33 +1,26 @@
 import { useRef, useState, useEffect } from "react";
-import "./Rope.scss"; // Asegúrate de tener un archivo de estilos CSS
-import "./Rope.scss"; // Asegúrate de tener un archivo de estilos CSS
+import "./Rope.scss";
 
 const Rope = () => {
   const objectRef = useRef(null);
-  const [position, setPosition] = useState({ x: 0, y: 360 });
+  const [position, setPosition] = useState(360);
 
   useEffect(() => {
     const handleMouseMove = (event: any) => {
-      const mouseX = event.clientX;
       const mouseY = event.clientY;
 
       const object: HTMLBRElement = objectRef.current!;
 
       if (object) {
         const objectRect = object.getBoundingClientRect();
-        const objectX = Math.floor(objectRect.left + objectRect.width / 2);
-        const objectY = Math.floor(objectRect.top + objectRect.height / 2);
+        const objectY = objectRect.top + objectRect.height;
 
-        const distance = Math.sqrt(
-          (mouseX - objectX) ** 2 + (mouseY - objectY) ** 2
-        );
-        const deltaY = ((mouseY - objectY) / distance) * 2;
-        if (distance <= 150) {
-          // console.log("position adentro:", position);
-          setPosition({
-            x: position.x,
-            y: position.y + deltaY,
-          });
+        const distance = objectY - mouseY;
+        const deltaY = (mouseY - objectY) / distance;
+        if (distance <= 100 && distance > 0) {
+          setPosition(position + deltaY);
+        } else if (distance >= -100 && distance < 0) {
+          setPosition(position - deltaY);
         }
       }
     };
@@ -37,13 +30,7 @@ const Rope = () => {
     };
   }, [position]);
 
-  return (
-    <div
-      id="line"
-      ref={objectRef}
-      style={{ left: position.x, top: position.y }}
-    ></div>
-  );
+  return <div id="line" ref={objectRef} style={{ top: position }}></div>;
 };
 
 export default Rope;
