29089e89122a37a13042fb2ec6100dc9e6acda6a
KevinAndresG <kevinandresg@gmail.com>
2024-04-29 01:28:35 -0500

    fixing clock in about

--- src/Components/Clock/Clock.tsx
+++ src/Components/Clock/Clock.tsx
@@ -10,51 +10,39 @@ const Clock = () => {
   let day = timeFormat[0];
   let month = timeFormat[1];
   let dayNumber = timeFormat[2];
-  let secondGrades = 0;
-  const [amPm, setAmPm] = useState("");
   const [hour, setHour] = useState(time[0]);
   const [minute, setMinute] = useState(time[1]);
-  const [second, setSecond] = useState("");
-  let hourGrades = 30 * parseInt(hour);
-  let minuteGrades = 6 * parseInt(minute);
+  const [second, setSecond] = useState(now.split(":")[2].split(" ")[0]);
+  const [defTime, setDefTime] = useState("");
+  const [secondGrades, setSecondGrades] = useState(6 * parseInt(second));
+  const [minuteGrades, setMinuteGrades] = useState(6 * parseInt(minute));
+  const [hourGrades, setHourGrades] = useState(30 * parseInt(hour));
   useEffect(() => {
-    console.log("This Is  time:", time);
-    let add = "";
-    let defTime = "";
     time.map((x) => {
       if (x.split(" ").length >= 2) {
-        add = x.split(" ").pop()!;
         setSecond(x.split(" ").shift()!);
-        secondGrades = 6 * parseInt(second);
-        setAmPm(add);
       }
     });
-    time.pop();
-    time.join(":");
-    defTime = time.join(":") + " " + add;
-    // setInterval(() => {
-    //   if (parseInt(second) === 60) {
-    //     setSecond("0");
-    //     secondGrades = 0;
-    //     let m = parseInt(minute);
-    //     m++;
-    //     setMinute(m.toString());
-    //     minuteGrades += 6;
-    //   }
-    //   if (parseInt(minute) === 60) {
-    //     setMinute("0");
-    //     minuteGrades = 0;
-    //     let h = parseInt(hour);
-    //     h++;
-    //     setHour(h.toString());
-    //     hourGrades += 6;
-    //   }
-    //   let s = parseInt(second);
-    //   s++;
-    //   setSecond(s.toString());
-    //   secondGrades += 6;
-    // }, 1000);
-    clearInterval;
+    let amPm = time.pop();
+    setDefTime(time.join(" : ") + " " + amPm!.split(" ").pop());
+    const myTimeout = setTimeout(() => {
+      setSecondGrades(6 * parseInt(second));
+      setSecond((parseInt(second) + 1).toString());
+      if (parseInt(second) >= 60) {
+        // setSecond("0");
+        // setSecondGrades(0);
+        setMinute((parseInt(minute) + 1).toString());
+        setMinuteGrades(6 * (parseInt(minute) + 1));
+      }
+      if (parseInt(minute) >= 60) {
+        // setMinute("0");
+        // setMinuteGrades(0);
+        setHour((parseInt(hour) + 1).toString());
+        setHourGrades(30 * (parseInt(hour) + 1));
+      }
+      clearTimeout(myTimeout);
+    }, 1000);
+    return;
   }, [second]);
 
   return (
@@ -64,20 +52,32 @@ const Clock = () => {
           <div className="visualClock">
             <div className="lines">
               <div className="center"></div>
-              {/* <div className="h"
-                        [ngStyle]="{'transform': 'translateY(' + -50 + '%) rotateZ(' + hourGrades + 'deg)'}"></div>
-                    <div className="m"
-                        [ngStyle]="{'transform': 'translateY(' + -50 + '%) rotateZ(' + minuteGrades + 'deg)'}"></div>
-                    <div className="s"
-                        [ngStyle]="{'transform': 'translateY(' + -50 + '%) rotateZ(' + secondGrades + 'deg)'}">
-                    </div> */}
+              <div
+                className="h"
+                style={{
+                  transform:
+                    "translateY(" + -50 + "%) rotateZ(" + hourGrades + "deg)",
+                }}
+              ></div>
+              <div
+                className="m"
+                style={{
+                  transform:
+                    "translateY(" + -50 + "%) rotateZ(" + minuteGrades + "deg)",
+                }}
+              ></div>
+              <div
+                className="s"
+                style={{
+                  transform:
+                    "translateY(" + -50 + "%) rotateZ(" + secondGrades + "deg)",
+                }}
+              ></div>
             </div>
           </div>
           <div className="dateClock">
             <div className="time">
-              <div className="hour">{hour}</div>
-              <div className="minute">{minute}</div>
-              <div className="amPm">{amPm}</div>
+              <div className="digit">{defTime}</div>
             </div>
             <div className="date">
               <div className="day">{day}</div>
