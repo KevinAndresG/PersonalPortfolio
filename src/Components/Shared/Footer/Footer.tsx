91294b45fb6262fbd98e20f843c52ee854ee97d9
KevinAndresG <kevinandresg@gmail.com>
2024-05-04 14:49:29 -0500

    fix menu bug and light footer and menu colors

--- src/Components/Shared/Footer/Footer.tsx
+++ src/Components/Shared/Footer/Footer.tsx
@@ -1,15 +1,21 @@
 import "./Footer.scss";
 
 const Footer = () => {
+  console.log(
+    "This Is  window.location.href:",
+    window.location.href.toLowerCase().includes("home")
+  );
   return (
-    <footer>
-      <p className="name">
-        <code>Kevin Andres Garcia velez</code>
-      </p>
-      <p className="front">
-        <code>Front End DEVELOPER</code>
-      </p>
-    </footer>
+    !window.location.href.toLowerCase().includes("home") && (
+      <footer>
+        <p className="name">
+          <code>Kevin Andres Garcia velez</code>
+        </p>
+        <p className="front">
+          <code>Front End DEVELOPER</code>
+        </p>
+      </footer>
+    )
   );
 };
 
